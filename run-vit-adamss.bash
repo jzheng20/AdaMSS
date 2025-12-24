be810e0e948c7e6267281bf99c287e85e3d9546e
Jingjing Zheng <103084414+jzheng20@users.noreply.github.com>
2025-12-24 09:51:05 -0800

    Update run-vit-adamss.bash

--- run-vit-adamss.bash
+++ run-vit-adamss.bash
@@ -76,7 +76,6 @@ mkdir -p log
 
 CUDA_VISIBLE_DEVICES=0 python exec_adamss.py \
     --model-name-or-path google/${model} \
-    --dataset_path  \
     --dataset-name cars \
     --target_KK 200 \
     --head_lr "$head_lr" \
