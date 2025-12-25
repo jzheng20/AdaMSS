d325e1e9b10f957de7aef0fa03627d09d73f9ef8
Jingjing Zheng <103084414+jzheng20@users.noreply.github.com>
2025-12-25 13:48:33 -0800

    Update NLU_GLUE_adamss.py

--- NLU_GLUE_adamss.py
+++ NLU_GLUE_adamss.py
@@ -72,7 +72,7 @@ for seed in seeds:
         tokenizer.pad_token_id = tokenizer.eos_token_id
     
  
-    datasets = load_dataset(args.dataset_path+"datasets_nlu/glue/glue/"+task)  
+    datasets = load_dataset(args.dataset_path+"glue/"+task)  
     #metric=load_metric("glue",task)  
     from evaluate import load
     metric = load("lrr_pkg/evaluate/metrics/glue/glue.py", task) 
